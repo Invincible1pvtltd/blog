0e9a97939a759cc29007f46e7e2b4f9d15a5fc1e
Invincible1.pvt.ltd <invincible1.pvt.ltd@gmail.com>
2024-10-28 21:02:49 +0530

    Add files via upload

---
+++ main/script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.getElementById('menu-button');
+    const sidePanel = document.getElementById('side-panel');
+    const mainContent = document.getElementById('home');
+    const photoContent = document.getElementById('photo');
+    const bodyContent = document.getElementById('body-content')
+    
+    // Open/Close Side Panel on Menu Button Click
+    menuButton.addEventListener('click', (e) => {
+        sidePanel.classList.toggle('-translate-x-full');  // Toggle panel slide-in/out
+        // mainContent.classList.toggle('blur-lg');  // Toggle blur effect on main content
+        e.stopPropagation();  // Prevent click from propagating to the document
+    });
+
+    // Close Side Panel if Clicking on Main Content
+    mainContent.addEventListener('click', () => {
+        if (!sidePanel.classList.contains('-translate-x-full')) { // Only close if panel is open
+            sidePanel.classList.add('-translate-x-full');  // Close panel
+            mainContent.classList.remove('blur-lg');  // Remove blur effect from main content
+        }
+    });
+
+    photoContent.addEventListener('click', () => {
+        if (!sidePanel.classList.contains('-translate-x-full')) { // Only close if panel is open
+            sidePanel.classList.add('-translate-x-full');  // Close panel
+            photoContent.classList.remove('blur-lg');
+        }
+    });
+
+    bodyContent.addEventListener('click', () => {
+        if (!sidePanel.classList.contains('-translate-x-full')) { // Only close if panel is open
+            sidePanel.classList.add('-translate-x-full');  // Close panel
+            bodyContent.classList.remove('blur-lg');  // Remove blur effect from main content
+        }
+    });
+});
